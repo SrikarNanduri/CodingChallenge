3a7b0d43ed719c4f3095d1166beeb22f7eb8e3d8
Srikar <srikarnanduri628@gmail.com>
2019-02-07 20:16:47 +0530

    Day2 Palindrome is Done.

--- src/main/java/DaysJava/Day2PalindromeCheck.java
+++ src/main/java/DaysJava/Day2PalindromeCheck.java
@@ -1,4 +1,18 @@
 package DaysJava;
 
+import java.util.Scanner;
+
 public class Day2PalindromeCheck {
+    public static void  main(String[] agrs){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("/* ===== Palindrome Check ===== */");
+        System.out.println("Enter the string to be checked: ");
+        String palindromeCheckString = scanner.nextLine().toLowerCase();
+        String reversedPalindromeCheckString = new StringBuilder(palindromeCheckString).reverse().toString();
+        if(palindromeCheckString.equals(reversedPalindromeCheckString)){
+            System.out.println("True - " + reversedPalindromeCheckString + " is equal to " + palindromeCheckString);
+        } else {
+            System.out.println("False - " + palindromeCheckString + " is not equal to " + reversedPalindromeCheckString);
+        }
+    }
 }
