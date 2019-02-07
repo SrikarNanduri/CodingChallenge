b015732e9af7a0c74225e0b3c675ac40e525c02c
Srikar <srikarnanduri628@gmail.com>
2019-02-07 20:21:54 +0530

    Day2 String reversal Done in Kotlin.

--- src/main/kotlin/DaysKotlin/Day2ReverseStringKt.kt
+++ src/main/kotlin/DaysKotlin/Day2ReverseStringKt.kt
@@ -1,2 +1,12 @@
 package DaysKotlin
 
+import java.util.*
+
+fun main(args: Array<String>){
+    println("/* ===== String Reversal ===== */")
+    val reader = Scanner(System.`in`)
+    println("Enter the string to be reversed: ")
+    val stringToBeReversed = reader.nextLine()
+    println(stringToBeReversed.reversed())
+
+}
