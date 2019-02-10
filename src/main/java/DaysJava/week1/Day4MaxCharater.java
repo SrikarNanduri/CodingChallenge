e8b39a5cdd2668819d4cea49c01e2b059a998a7b
Srikar <srikarnanduri628@gmail.com>
2019-02-10 18:02:49 +0530

    Day 4 max charater code for Java and Kotlin Done.

---
+++ src/main/java/DaysJava/week1/Day4MaxCharater.java
@@ -0,0 +1,33 @@
+package DaysJava.week1;
+
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Day4MaxCharater {
+
+        static void findMaximumOccurring(String input){
+            HashMap<Character, Integer> map = new HashMap<>();
+            char[] chars = input.toCharArray();
+            for (int i = 0; i <chars.length ; i++) {
+                char c = chars[i];
+                if(map.containsKey(c)){
+                    int count = map.get(c);
+                    count++;
+                    map.put(c, count);
+                }else{
+                    map.put(c, 1);
+                }
+            }
+            System.out.println(map);
+            Character key = map.entrySet().stream().max((o1, o2) -> o1.getValue() > o2.getValue()? 1 : -1).get().getKey();
+            System.out.println("The max occuring charater is " + key + " and no of time it occured is " + map.get(key));
+        }
+
+        public static void main(String[] args) {
+            System.out.println("Enter a string:");
+            String input = new Scanner(System.in).nextLine();
+            System.out.println("Input- " + input);
+            findMaximumOccurring(input);
+        }
+}
---
+++ src/main/kotlin/DaysKotlin/week1/Day4MaxCharater.kt
@@ -0,0 +1,28 @@
+package DaysKotlin.week1
+
+import java.util.*
+
+fun main(args: Array<String>){
+val reader = Scanner(System.`in`)
+    println("Enter a string: ")
+    val string = reader.nextLine()
+
+    var map : MutableMap<Char, Int> = mutableMapOf()
+    var charaters = string.toCharArray()
+    for (i in 0 until charaters.size){
+        var c = charaters[i]
+        if(map.containsKey(c)){
+                var count = map[c]
+            if(count != null) {
+                count += 1
+                map.put(c, count)
+            }
+            } else {
+            map.put(c, 1)
+        }
+    }
+    println(map)
+    val key = map.maxBy { it.value }!!.key
+    var value =  map[key]
+    println("The max occuring charater is $key and no of time it occured is $value")
+}
