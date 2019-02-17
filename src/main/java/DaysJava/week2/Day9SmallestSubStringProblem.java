981f649df2cc2540baad21d94f2e10e8cb26b3c0
Srikar <srikarnanduri628@gmail.com>
2019-02-17 13:50:48 +0530

    Day9 Java code done

--- src/main/java/DaysJava/week2/Day9SmallestSubStringProblem.java
+++ src/main/java/DaysJava/week2/Day9SmallestSubStringProblem.java
@@ -1,8 +1,5 @@
 package DaysJava.week2;
 
-import com.google.common.collect.Multimap;
-import com.google.common.collect.TreeMultimap;
-
 import java.util.Scanner;
 
 public class Day9SmallestSubStringProblem {
@@ -16,20 +13,44 @@ public class Day9SmallestSubStringProblem {
         for(int i=0;i<n;i++)
             chars[i]=scanner.next().charAt(0);
 
-        smallestSubString(chars, string);
+        generate(string, chars);
     }
 
-    private static void smallestSubString(char[] chars, String string ){
+    private static boolean check(String s, char[] c)
+    {
+        String str=new String(c);
+        int k=0;
+        for(int i=0;i<c.length;i++)
+        {
+            if(s.contains(str.substring(i,i+1)))
+                k++;
+        }
+        if(k==c.length)
+            return true;
+        else
+            return false;
+    }
 
-        char[] stringArray = string.toLowerCase().toCharArray();
-        Multimap<Character, Integer> indexes = TreeMultimap.create();
-        for (char aChar : chars) {
-            for (int j = 0; j < string.length(); j++) {
-                if (aChar == stringArray[j]) {
-                    indexes.put(aChar, j);
+    private static void generate(String str, char c[]) {
+        String s;
+        int min;
+        min=str.length();
+        String s1="";
+        for(int i=0;i<=str.length()-1;i++)
+        {
+            for(int j=i+1;j<=str.length();j++)
+            {
+                s=str.substring(i,j);
+                if(check(s,c))
+                {
+                    if(min>=s.length())
+                    {
+                        min=s.length();
+                        s1=s;
+                    }
                 }
             }
         }
-        System.out.println(indexes);
+        System.out.println(s1);
     }
 }
