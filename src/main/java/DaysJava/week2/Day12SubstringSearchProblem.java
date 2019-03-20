c7e98f9cb91508be49a9dcffa4877dca3d90001f
srikarn <srikarnanduri628@gmail.com>
2019-03-20 10:00:57 +0530

    Day 12 Brute Force done in Java.

--- src/main/java/DaysJava/week2/Day12SubstringSearchProblem.java
+++ src/main/java/DaysJava/week2/Day12SubstringSearchProblem.java
@@ -13,11 +13,30 @@ public class Day12SubstringSearchProblem {
         var string = scanner.nextLine();
         System.out.println("Enter a substring to find if present inside the given string:");
         var substring = scanner.nextLine();
-        System.out.println(substringSearchProblem(string, substring));
+        System.out.println("General Method - substring exists and is at position " +substringSearchProblem(string, substring));
+        bruteForceApproach(string, substring);
     }
 
     private static int substringSearchProblem(String string, String substring) {
         return string.indexOf(substring);
+    }
+
+    private static void bruteForceApproach(String string, String substring){
+        String str; int position = -1;
+        for(int i =0; i < string.length(); i++){
+          if(string.charAt(i) == substring.charAt(0)){
+              for(int j =1; j < substring.length(); j++){
+                  if(string.charAt(i+j) != substring.charAt(j)){
+                      break;
+                  }
+                  if(j == substring.length() - 1) {
+                      position = i;
+                  }
+              }
+          }
+        }
+        System.out.println("Brute Force - substring exists and is at position " + position);
+
     }
 
 }
